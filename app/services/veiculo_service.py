fe74ce61cb0bd6e918581d802727643d37e7e2ca
EduardoPSNeri <eduardopereirasilvaneri2013@gmail.com>
2026-08-18 09:27:06 -0300

    implementação de cadastro de veiculo

--- app/services/veiculo_service.py
+++ app/services/veiculo_service.py
@@ -3,7 +3,13 @@ Contém todas as regras de negócio da aplicação.
 
 Coordena as operações e utiliza o Repository para acessar o banco.
 """
-from app.repositories.veiculo_repository import *
+from app.models.veiculo_model import Veiculo
+from app.repositories.veiculo_repository import (
+    veiculos,
+    buscar_placa,
+    buscar_ultimo_numero_frota
+)
+
 
 def gerar_numero_frota(buscar_ultimo_numero_frota,):
     if buscar_ultimo_numero_frota is None:
@@ -14,16 +20,28 @@ def veiculo_existe (placa):
     if placa is not None:
         return "Já existe um veículo cadastrado com esta placa."
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-'''    veiculo_existente = buscar_placa(veiculos, placa)
 
+def cadastrar_veiculo(veiculos):
+    
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+"""
+veiculo_existente = buscar_placa(veiculos, placa)
     if veiculo_existente is not None:
-        return "Já existe um veículo cadastrado com esta placa."'''
+        return "Já existe um veículo cadastrado com esta placa." 
+"""
