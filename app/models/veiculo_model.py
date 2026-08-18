eb1b320e1194571229f2a9e5bcf9f6abb5e574eb
EduardoPSNeri <eduardopereirasilvaneri2013@gmail.com>
2026-08-18 09:40:31 -0300

    otimização de codigos obsoletos

--- app/models/veiculo_model.py
+++ app/models/veiculo_model.py
@@ -3,10 +3,9 @@ Representa a estrutura da tabela no banco de dados.
 
 Define colunas, tipos de dados e relacionamentos entre tabelas.
 """
-
 class Veiculo:
 
-    def __init__(self, numero_frota, placa, marca, modelo, km,):
+    def __init__(self, numero_frota, placa, marca, modelo, km):
         self.numero_frota = numero_frota
         self.placa = placa
         self.marca = marca
--- app/services/veiculo_service.py
+++ app/services/veiculo_service.py
@@ -1,24 +1,12 @@
-"""
-Contém todas as regras de negócio da aplicação.
-
-Coordena as operações e utiliza o Repository para acessar o banco.
-"""
 from app.models.veiculo_model import Veiculo
 from app.repositories.veiculo_repository import *
 
 
-##GERA O PROXIMO NUMERO DA FROTA 
-def gerar_numero_frota(buscar_ultimo_numero_frota):
-    if buscar_ultimo_numero_frota is None:
+def gerar_numero_frota(ultimo_numero_frota):
+    if ultimo_numero_frota is None:
         return 1
-    return buscar_ultimo_numero_frota + 1 
 
-
-##RETORNA SE A PLACA EXISTE OU NAO
-def veiculo_existe (placa):
-    if placa is not None:
-        return "Já existe um veículo cadastrado com esta placa."
-    
+    return ultimo_numero_frota + 1
 
 
 def cadastrar_veiculo(veiculos, placa, marca, modelo, km):
@@ -30,18 +18,18 @@ def cadastrar_veiculo(veiculos, placa, marca, modelo, km):
 
     ultimo_numero_frota = buscar_ultimo_numero_frota(veiculos)
     proximo_numero = gerar_numero_frota(ultimo_numero_frota)
-    
-    
-    novo_veiculo = Veiculo(proximo_numero, placa, marca, modelo, km)
-    
+
+    novo_veiculo = Veiculo(
+        proximo_numero,
+        placa,
+        marca,
+        modelo,
+        km
+    )
+
     adicionar_veiculo(veiculos, novo_veiculo)
+
     return "Veículo cadastrado com sucesso"
-
-    
-    
-
-
-
 
 
 
