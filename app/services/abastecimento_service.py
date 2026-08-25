811aa98c5b43c76aaf842e72ff90432cb59d9ff4
EduardoPSNeri <eduardopereirasilvaneri2013@gmail.com>
2026-08-25 11:29:09 -0300

    atualização de custo por km

--- app/services/abastecimento_service.py
+++ app/services/abastecimento_service.py
@@ -16,8 +16,6 @@ def validar_quantidade_litro(quantidade_litro):
     return True, "Quantidade de Litros valida"
 
 
-
-
 def cadastrar_abastecimento(abastecimentos,veiculos,placa,data,km,combustivel,valor_litro,quantidade_litro):
 
     veiculo_encontrado = buscar_placa(veiculos, placa)
@@ -131,38 +129,47 @@ def calcular_distancia_historico(historico):
     distancia_percorrida = ultimo_km - inicial_km
     
     return distancia_percorrida
- 
 
-def calcular_custo_por_km(historico):
+
+def calcular_media_custo_por_km(historico):
+
+    soma_custos = 0
+    quantidade = 0
+
+    for abastecimento in historico:
+
+        if abastecimento.custo_por_km is not None:
+            soma_custos += abastecimento.custo_por_km
+            quantidade += 1
+            
+    if quantidade == 0 :
+         return None
+        
+    return soma_custos / quantidade
+        
     
-   distancia = calcular_distancia_historico(historico)
-   
-   if distancia is None:
-       return None
-   
-   total_gasto = calcular_total_gasto(historico)
-   
-   custo_por_km = total_gasto / distancia
-   
-   return custo_por_km
-
+    
+    
+    
+    
+    
+    
 
 #relatorios
-
 def gerar_resumo_abastecimento(historico):
     
     total_gasto = calcular_total_gasto(historico)
     total_litros = calcular_total_litros(historico)
     quantidade = quantidade_abastecimento(historico)
     media_consumo =calcular_media_consumo_geral(historico)
-    custo_km = calcular_custo_por_km(historico)
+    media_custo_km = calcular_media_custo_por_km(historico)
     
     return{
         "Total_Gasto": total_gasto,
         "Total_abastecido": total_litros,
         "Quantidade_abastecimento":quantidade,
         "Media_Consumo":media_consumo,
-        "Custo_KM": custo_km,
+        "Media_Custo_KM": media_custo_km,
         
     }
     
